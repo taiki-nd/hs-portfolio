4718cfa2924d6c412cc403b9f0eb867dfb53d7de
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-05 20:32:07 +0900

    カテゴリーモデルの作成

---
+++ app/models/category.rb
@@ -0,0 +1,12 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'ポートレート' },
+    { id: 3, name: '風景' },
+    { id:　4, name: 'オブジェクト' }
+  ]
+ 
+   include ActiveHash::Associations
+   has_many :posts
+ 
+  end
