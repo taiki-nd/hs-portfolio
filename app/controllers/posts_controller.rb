bfa6b2ec3daa389d6715804ebbad4acd9b6f09fd
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-05 23:45:11 +0900

    indexページの表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.order("created_at DESC")
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :category_id, :model_name, :model_url, :text, :image)
+  end
+
 end
