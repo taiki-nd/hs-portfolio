60310e01bcfdaa488345be893a51b1154600cb47
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-09 17:24:44 +0900

    editアクション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   
   def index
     @posts = Post.order("created_at DESC")
@@ -18,13 +20,19 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :category_id, :model_name, :model_url, :text, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
