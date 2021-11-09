60ea1df9495616c4bb95827f0e36423756fb0255
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-09 16:59:35 +0900

    createアクション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,15 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def post_params
