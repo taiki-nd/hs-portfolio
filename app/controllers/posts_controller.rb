8f0d7a88af08696a62fabfe1b03d20d204bd9938
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-10 03:19:17 +0900

    ログアウトボタンの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   
   def index
     @posts = Post.order("created_at DESC")
