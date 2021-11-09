69aef98979fbed7c119f8f6101ba9a3df528d125
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-09 20:15:08 +0900

    初期データの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+11.times do
+  Post.create!(
+    image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/profile.jpg")),filename: 'profile.jpg'),
+    title: "title",
+    category_id: 3,
+    name: "user",
+    url: "https://google.com",
+    text: "texttexttexttext texttexttexttext texttext texttext texttexttexttexttexttext texttexttexttext texttext"
+  )
+end
