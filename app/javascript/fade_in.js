8233208c2b0263f4470356292d70b32c2c668877
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-10 17:21:31 +0900

    プロフィールの動き

--- app/javascript/fade_in.js
+++ app/javascript/fade_in.js
@@ -11,6 +11,24 @@ $(function(){
           $(this).css('transform', 'translateY(0)');
         }
       });
+      $('.faderight').each(function(){
+        let scroll = $(window).scrollTop();
+        let target = $(this).offset().top;
+        let windowHeight = $(window).height();
+        if(scroll > target - windowHeight + 200){
+          $(this).css('opacity','1');
+          $(this).css('transform', 'translateY(0)');
+        }
+      });
+      $('.fadeleft').each(function(){
+        let scroll = $(window).scrollTop();
+        let target = $(this).offset().top;
+        let windowHeight = $(window).height();
+        if(scroll > target - windowHeight + 200){
+          $(this).css('opacity','1');
+          $(this).css('transform', 'translateY(0)');
+        }
+      });
     });
   }else{
     $(window).scroll(function(){
